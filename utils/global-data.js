6fd6ed9cf9335361dbd713a879df9214af68a27b
John Doe <john.doe@contoso.com>
2023-05-01 10:31:58 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Jon Silvester';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Cyber & InfoSec Blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
